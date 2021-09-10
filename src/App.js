b4d01dfc6b24fdb5dfb6eb5596b56236df91a86a
Adrian Antanon Orozco <aantanon@brothapps.com>
2021-09-10 11:52:19 +0200

    feat: :zap: add links & navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Switch } from "react-router-dom";
 
 import { AllMeetupsPage, FavoritesPage, NewMeetupPage } from "./pages";
+import { MainNavigation } from "./components/layout";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route exact path="/">
           <AllMeetupsPage />
